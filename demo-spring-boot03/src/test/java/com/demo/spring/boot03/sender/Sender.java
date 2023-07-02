21f99856409f33de4936fdf7abb2013be447e7de
xial-a <xial-a@glodon.com>
2023-07-02 16:39:49 +0800

    refactor: code.

--- demo-spring-boot03/src/test/java/com/demo/spring/boot03/sender/Sender.java
+++ demo-spring-boot03/src/test/java/com/demo/spring/boot03/sender/Sender.java
@@ -1,28 +1,25 @@
 package com.demo.spring.boot03.sender;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
-@RunWith(SpringRunner.class)
 public class Sender {
-  @Autowired
-  AmqpTemplate amqpTemplate;
+    @Autowired
+    AmqpTemplate amqpTemplate;
 
-  //exchange交换器名称
-  @Value("${mq.config.exchange.topic}")
-  private String topicExchange;
+    //exchange交换器名称
+    @Value("${mq.config.exchange.topic}")
+    private String topicExchange;
 
-  @Value("${mq.config.route.topic}")
-  private String topicRouteName;
+    @Value("${mq.config.route.topic}")
+    private String topicRouteName;
 
-  @Test
-  public void send() throws InterruptedException {
-    amqpTemplate.convertAndSend(topicExchange, topicRouteName, "product...debug========" + "Hello");
-  }
+    @Test
+    public void send() throws InterruptedException {
+        amqpTemplate.convertAndSend(topicExchange, topicRouteName, "product...debug========" + "Hello");
+    }
 }
