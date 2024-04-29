d24e27c25023f3040fda8dcd4169e1ffaa46bf6d
xial-a <xial-a@glodon.com>
2024-04-29 22:49:04 +0800

    refactor： update to jdk 21

--- demo-spring-boot06/src/main/java/com/demo/spring/boot06/config/EhCacheConfig.java
+++ demo-spring-boot06/src/main/java/com/demo/spring/boot06/config/EhCacheConfig.java
@@ -1,52 +1,41 @@
 package com.demo.spring.boot06.config;
 
-import org.ehcache.Cache;
 import org.ehcache.CacheManager;
+import org.ehcache.config.CacheConfiguration;
 import org.ehcache.config.builders.CacheConfigurationBuilder;
 import org.ehcache.config.builders.CacheManagerBuilder;
 import org.ehcache.config.builders.ExpiryPolicyBuilder;
 import org.ehcache.config.builders.ResourcePoolsBuilder;
+import org.ehcache.jsr107.EhcacheCachingProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.cache.CacheProperties;
+import org.springframework.boot.autoconfigure.cache.JCacheManagerCustomizer;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.time.Duration;
-import java.time.temporal.ChronoUnit;
 
 @Configuration
 @ConditionalOnProperty(name = "cache.type", havingValue = "ehcache")
 public class EhCacheConfig {
 
-//  @Bean
-//  public CacheManager cacheManager() {
-
-//    net.sf.ehcache.CacheManager ehCacheManager = new net.sf.ehcache.CacheManager();
-//    ehCacheManager.addCache("cache1");
-//    ehCacheManager.addCache("cache2");
-//    EhCacheCacheManager cacheManager = new EhCacheCacheManager();
-//
-//    cacheManager.setCacheManager(ehCacheManager);
-//
-//    System.out.println("Ehcache cache init.");
-//
-//    return cacheManager;
-
+  @Autowired
+  private CacheProperties cacheProperties;
 
   @Bean
   public CacheManager cacheManager() {
-    CacheManager cacheManager = CacheManagerBuilder.newCacheManagerBuilder().build(true);
+    CacheManagerBuilder builder = CacheManagerBuilder.newCacheManagerBuilder();
 
-    CacheConfigurationBuilder<String, String> cacheConfig = CacheConfigurationBuilder.newCacheConfigurationBuilder(String.class, String.class,
-            // 根据需要配置资源池
-            ResourcePoolsBuilder.heap(100)
-        )
-        // 设置过期策略
-        .withExpiry(ExpiryPolicyBuilder.timeToLiveExpiration(Duration.of(10, ChronoUnit.MINUTES)));
+    // 配置一个缓存
+    builder.withCache("cache1",
+        CacheConfigurationBuilder.newCacheConfigurationBuilder(String.class, String.class,
+                ResourcePoolsBuilder.heap(100))
+            .withExpiry(ExpiryPolicyBuilder.timeToLiveExpiration(Duration.ofMinutes(1)))
+            .build());
 
-    cacheManager.createCache("cache1", cacheConfig);
-    cacheManager.createCache("cache2", cacheConfig);
-
-    System.out.println("Ehcache cache init.");
+    CacheManager cacheManager = builder.build();
+    cacheManager.init();
     return cacheManager;
   }
 }
